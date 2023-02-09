08d51e5af6ca1e9ab38ccd48dffbdc5527ecda92
DBrdak <brdak02dominik@gmail.com>
2023-02-09 17:46:52 +0100

    Client Image Upload

---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,33 @@
+import { observer } from 'mobx-react-lite'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Grid, GridColumn } from 'semantic-ui-react'
+import LoadingComponent from '../../App/layout/LoadingComponent'
+import { useStore } from '../../App/stores/store'
+import ProfileContent from './ProfileContent'
+import ProfileHeader from './ProfileHeader'
+
+function ProfilePage() {
+  const {username} = useParams<{username: string}>()
+  const {profileStore} = useStore()
+  const {loadProfile, loadingProfile, profile} = profileStore
+
+  useEffect(() => {
+    if(username) loadProfile(username)
+  }, [loadProfile, username])
+  
+  if(loadingProfile) return <LoadingComponent content='Loading profile...' />
+
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        {profile && 
+        <ProfileHeader profile={profile} />}
+        {profile &&
+        <ProfileContent profile={profile} />}
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default observer(ProfilePage)
